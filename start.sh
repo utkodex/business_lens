f97d412cf949904790b66ef34ae081c31a33229c
utkodex <bizzboosterdata@gmail.com>
2026-04-30 16:36:54 +0530

    fix: add missing shebang to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Start FastAPI backend in background on a fixed internal port (8001).
 # We MUST override PORT because Uvicorn automatically binds to $PORT if it exists.
 PORT=8001 uvicorn app:app --host 0.0.0.0 --port 8001 &
